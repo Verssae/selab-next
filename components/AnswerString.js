523b805e75a5b19ae496d13bc222ee93cfc796c9
Jaewoo Yang <sweyjw@gmail.com>
2019-12-20 02:56:02 +0900

    Merge Updated Quiz Component (#16)
    * Implement QuizList Style
    
    * Add Quiz Contents Component
    
    * Change quiz schema

--- components/AnswerString.js
+++ components/AnswerString.js
@@ -1,28 +1,46 @@
 /** @jsx jsx */
 import { jsx, css, Global } from "@emotion/core"
-import { useQuery, useSubscription } from '@apollo/react-hooks'
+import {useRef} from "react"
+import { useQuery, useMutation } from '@apollo/react-hooks'
 import { useState, useEffect } from 'react'
 import theme from "./theme"
 import styled from "@emotion/styled"
 
 import TextareaAutosize from 'react-textarea-autosize';
+import { ADD_ANSWER } from '../lib/query'
 
-function AnswerString() {
+function AnswerString({ quizId }) {
+  // const [content, setContent] = useState('')
+  const [addAnswer, { data }] = useMutation(ADD_ANSWER)
+  const ref = useRef()
+
   return (
-    <form>
-      <TextareaAutosize autoFocus placeholder="Input your answer!" rows="2" css={css`
-        width: 100%;
-        font-size: 24px;
-        resize: none;
-        padding: 3px;
-        border: 1px solid #D0D3D4;
-      `}/>
-      <div>
-        <input type="submit" css={css`
-          float:right;
-        `}></input>
-      </div>
+    <form onSubmit={e => {
+      e.preventDefault();
+      addAnswer({ variables: {
+        quizId: quizId,
+        userId: "ck4cwzpm201bm0790454i1uz0",
+        content: ref.current.value
+      }})
+      ref.current.value = ''
+    }}>
+      <Textarea inputRef={ref} autoFocus placeholder="Input your answer!" rows="2" />
+      <Sumbit/>
     </form>
+  )
+}
+
+const Textarea = styled(TextareaAutosize)`
+  width: 100%;
+  font-size: 24px;
+  resize: none;
+  padding: 3px;
+  border: 1px solid #D0D3D4;
+`
+
+const Sumbit = () => {
+  return (
+    <button type="submit">Submit</button>
   )
 }
 
