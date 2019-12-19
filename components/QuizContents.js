209896dabad51a781ae3078bbedaf9aa7397e7da
onnoo <sweyjw@gmail.com>
2019-12-20 04:20:04 +0900

    Complete Essay Quiz type

--- components/QuizContents.js
+++ components/QuizContents.js
@@ -6,14 +6,39 @@ import theme from "./theme"
 import styled from "@emotion/styled"
 
 import BackButtonIcon from './icons/BackButtonIcon'
-import { GET_QUIZ_CONTENTS } from '../lib/query'
+import { GET_QUIZ_CONTENTS, NEW_ANSWER_SUBSCRIPTION } from '../lib/query'
 import AnswerString from './AnswerString'
 
 function QuizContents({ id, setter }) {
-  const [state, setState] = useState(true)
+  const [answerList, setAnswerList] = useState([])
+
   const { loading, error, data } = useQuery(
-    GET_QUIZ_CONTENTS, { variables: { id: id } }
+    GET_QUIZ_CONTENTS, {
+      variables: { id: id },
+      fetchPolicy: "no-cache"
+    }
   )
+
+  useSubscription(
+    NEW_ANSWER_SUBSCRIPTION,
+    {
+      onSubscriptionData: ({subscriptionData}) => {
+        if (data) {
+          setAnswerList((answerList) => [
+            subscriptionData.data.newAnswer, ...answerList
+          ])
+        }
+      },
+      variables: {
+        quizId: id
+      }
+    }
+  )
+  useEffect(()=>{
+    if (data) {
+      setAnswerList(data.quiz.answers)
+    }
+  },[data])
 
   if (loading) return <p>Loading...</p>
   const { quiz } = data
@@ -27,7 +52,7 @@ function QuizContents({ id, setter }) {
         <p>{quiz.description}</p>
         <AnswerString quizId={id} />
         <ul>
-        {quiz.answers.map(({id, user, content, createdAt}) => (
+        {answerList.map(({id, user, content, createdAt}) => (
           <li key={id}>
             {user.user_id} - {createdAt}<br/>{content}
           </li>
